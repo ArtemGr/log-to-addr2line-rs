cc468f4b4901a90c135bd30f2ac77630a7c113e7
Artemciy <artemciy@gmail.com>
2019-09-10 00:22:27 +0300

    Handle ips (iOS) logs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+//#[macro_use] extern crate lazy_static;
 #[macro_use] extern crate unwrap;
 
+use gstuff::binprint;
 use regex::{Captures, Regex};
 use std::fs;
 use std::io::{self, BufRead};
@@ -15,9 +17,13 @@ struct Opt {
     /// Log file.
     #[structopt(short, long)]
     log: String,
-    /// How to run `addr2line`.
+    /// How to run "addr2line".
     #[structopt(long)]
-    addr2line: Option<String>
+    addr2line: Option<String>,
+    /// How to run "rustfilt".  
+    /// (Install it with "cargo install rustfilt").
+    #[structopt(long)]
+    rustfilt: Option<String>
 }
 
 fn android_ (opt: &Opt, line: &str, caps: Captures) {
@@ -40,6 +46,60 @@ fn android_ (opt: &Opt, line: &str, caps: Captures) {
     if !status.success() {println! ("{:?}", status)}
 }
 
+fn ips_ (opt: &Opt, line: &str, caps: Captures) {
+    let _binary_immage_name = unwrap! (caps.get (1)) .as_str();
+    let address = unwrap! (caps.get (2)) .as_str();
+    let load_address = unwrap! (caps.get (3)) .as_str();
+
+    println!();
+    println! ("{}", line);
+
+    let mut cmd = Command::new ("atos");
+    cmd.arg ("-arch") .arg ("arm64");
+    cmd.arg ("-o") .arg (&opt.exe);
+    cmd.arg ("-l") .arg (load_address);
+    cmd.arg (address);
+    let output = unwrap! (cmd.output(), "!atos");
+
+    if !output.stderr.is_empty() {
+        println! ("atos error: {}", binprint (&output.stderr, b'.'));
+        return
+    }
+
+    if !output.status.success() {
+        println! ("atos error");
+        return
+    }
+
+    let stdout = unwrap! (String::from_utf8 (output.stdout), "!utf-8");
+    let stdout = stdout.trim();
+    println! ("{}", stdout);
+
+    /*
+    lazy_static! {
+        static ref RS: Regex = unwrap! (Regex::new (r" \(\w+\.rs:\d+\)$"));
+    }
+    if !RS.is_match (stdout) {
+        println! ("{}", stdout);
+        return
+    }
+
+    let raw_symbol = stdout;
+    let rustfilt = opt.rustfilt.as_ref().map (|s| &s[..]) .unwrap_or ("rustfilt");
+    let mut cmd = Command::new (rustfilt);
+    cmd.stdin (Stdio::piped());
+    cmd.stdout (Stdio::piped());
+    let mut rustfilt = unwrap! (cmd.spawn(), "!rustfilt");
+    let mut stdin = unwrap! (rustfilt.stdin.take().ok_or ("!stdin"));
+    let mut stdout = unwrap! (rustfilt.stdout.take().ok_or ("!stdin"));
+    unwrap! (stdin.write_all (raw_symbol.as_bytes()));
+    drop (stdin);
+    assert! (unwrap! (rustfilt.wait()) .success());
+    let mut buf = String::new();
+    unwrap! (stdout.read_to_string (&mut buf));
+    println! ("filtered: {}", buf);*/
+}
+
 fn main() {
     let opt = Opt::from_args();
 
@@ -50,11 +110,18 @@ fn main() {
     // 08-06 05:24:35.985 21317 21317 F DEBUG   :     #00 pc 00123456  /data/data/package/app-folder/binary
     let android = unwrap! (Regex::new (r"^\d+-\d+ \d+:\d+:\d+\.\d+ \d+ \d+ F DEBUG +: +#(\d+) pc (\w+) +/"));
 
+    // Standard iOS crash log, such as "Runner-2019-09-09-182634.ips".
+    // 0   libsystem_kernel.dylib        	0x00000001b42d9c60 0x1b42c1000 + 101472
+    // https://developer.apple.com/library/archive/technotes/tn2151/_index.html#//apple_ref/doc/uid/DTS40008184-CH1-SYMBOLICATE_WITH_ATOS
+    let ips = unwrap! (Regex::new (r"^\d+\s+([\w\.]+)\s+(0x[0-9a-f]+) (0x[0-9a-f]+) \+ \d+"));
+
     for line in log_file.lines() {
         let line = match line {Ok (l) => l, Err (err) => {
             eprintln! ("Error getting a log line: {}", err); continue}};
         if let Some (caps) = android.captures (&line) {
             android_ (&opt, &line, caps)
+        } else if let Some (caps) = ips.captures (&line) {
+            ips_ (&opt, &line, caps)
         }
     }
 }
